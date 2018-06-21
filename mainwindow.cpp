4807dd019b112ce55dbe491e1c56d81daca516f4
bonaventureli <lfwendula@msn.cn>
2018-06-20 19:04:26 -0700

    nobackgrand

---
+++ mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDir>
+#include <QWSServer>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+// QWSServer::setBackground(QColor(0,0,0,0));
+ QPixmap pixmap(":/img/456.png");
+ QPalette palette;
+ palette.setBrush(backgroundRole(), QBrush(pixmap));
+ setPalette(palette);
+ setAttribute(Qt::WA_TranslucentBackground);//set backgroud tou ming
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
